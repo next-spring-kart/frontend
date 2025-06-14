09971eac1c734cb375b87448795506a80d42c7c7
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2025-06-14 20:20:04 +0530

    feat: added nextJs

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+
+const geistSans = Geist({
+	variable: '--font-geist-sans',
+	subsets: ['latin']
+})
+
+const geistMono = Geist_Mono({
+	variable: '--font-geist-mono',
+	subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+	title: 'Next Spring Kart',
+	description:
+		'A modern eCommerce web application built with Next.js for a lightning-fast, SEO-optimized frontend and Spring Boot'
+}
+
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+		</html>
+	)
+}
